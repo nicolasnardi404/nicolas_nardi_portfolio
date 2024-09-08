5957b5fe06f20357a3ddb64dbf48fa58a9ecfb02
esquizo ciborgue <nicolasnardi404@gmail.com>
2024-09-08 22:37:35 +0200

    secured path

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Projects from "./components/Projects";
 import styles from "./styles/publicDiary.module.css";
@@ -8,17 +9,24 @@ import "./styles/ProjectModal.css";
 import "./styles/ProjectCard.css";
 
 export default function Home() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Check if there's a token in localStorage
+    const token = localStorage.getItem("token");
+    setIsLoggedIn(!!token);
+  }, []);
+
   return (
     <div>
       <nav className={styles.nav}>
-        <Link href="/login" className={styles.navLink}>
-          Go to Login
-        </Link>
-        <Link href="/diary" className={styles.navLink}>
-          My Diary
-        </Link>
+        {isLoggedIn && (
+          <Link href="/diary" className={styles.navLink}>
+            Write
+          </Link>
+        )}
         <Link href="/public-diary" className={styles.navLink}>
-          Public Diary
+          Code Diary
         </Link>
       </nav>
       <Projects />
