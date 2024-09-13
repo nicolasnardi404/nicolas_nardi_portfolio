332454d2adf933a09ef9e4787e703d1f9edf8090
esquizo ciborgue <nicolasnardi404@gmail.com>
2024-09-13 22:30:49 +0200

    add favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
 import "./styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Analytics />
     </html>
   );
 }
